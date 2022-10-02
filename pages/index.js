90d6514433249696ac474d57f143a5d3749b3591
Tobi <tobi.bankole@gmail.com>
2022-10-02 21:44:27 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Philkeyz Official!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Website is coming soon" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Watch this space. check out philkeyz socials @philkeyz</code>
         </p>
       </main>
 
